e424a72941a91df3d1285e04dbb95c1b84eda065
Yakir Adela <yakirade@gmail.com>
2025-07-03 12:39:21 +0300

    Update secretmanager.tf

--- task-manager-devops/infra/secretmanager.tf
+++ task-manager-devops/infra/secretmanager.tf
@@ -1,12 +1,9 @@
-resource "aws_secretsmanager_secret" "db" {
-    name = "task-api-db-secret"
-}
 
 resource "aws_secretsmanager_secret_version" "db_version" {
-    secret_id           = aws_secretsmanager_secret.db.id 
-    secret_string       = jsondecode({
-        POSTGRES_USER       = "postgres"
-        POSTGRES_PASSWORD   = "supersecure"
-    }) 
-  
+  secret_id     = aws_secretsmanager_secret.db.id
+  secret_string = jsonencode({
+    POSTGRES_USER     = "postgres"
+    POSTGRES_PASSWORD = "supersecure"
+  })
 }
+
