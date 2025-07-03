4a3e11d8804609b9e5e93716e77a15475d7ef94c
Yakir Adela <yakirade@gmail.com>
2025-07-03 12:40:43 +0300

    Update rds.tf

--- task-manager-devops/infra/rds.tf
+++ task-manager-devops/infra/rds.tf
@@ -1,9 +1,9 @@
-resource "aws_db_instance" "posgres" {
-    allocated_storage       = 20
-    engine                  = "postgres"
-    instance_class          = "db.t3.micro"
-    name                    = "tasks"
-    username                = "postgres"
-    password                = "secret"
-    skip_final_snapshot     = true     
+resource "aws_db_instance" "postgres" {
+  allocated_storage       = 20
+  engine                  = "postgres"
+  instance_class          = "db.t3.micro"
+  db_name                 = "tasks"
+  username                = "postgres"
+  password                = "secret"
+  skip_final_snapshot     = true
 }
