33ec47f44313a575a8f01ed99d1507a17717e203
Yakir Adela <yakirade@gmail.com>
2025-07-03 12:11:50 +0300

    Update variables.tf

--- task-manager-devops/infra/variables.tf
+++ task-manager-devops/infra/variables.tf
@@ -1,4 +1,10 @@
 variable "cluster_name" {
     default = "task-cluster"
-  
+}
+
+
+variable "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "task-api"
 }
