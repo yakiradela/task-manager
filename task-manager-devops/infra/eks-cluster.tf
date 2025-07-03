4c2f81f9d3fbf9d81b4ab37ae7e49d58a2226ef2
Yakir Adela <yakirade@gmail.com>
2025-07-03 12:41:45 +0300

    Update eks-cluster.tf

--- task-manager-devops/infra/eks-cluster.tf
+++ task-manager-devops/infra/eks-cluster.tf
@@ -1,6 +1,33 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name = "task-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+}
+
 module "eks" {
-    source          = "terraform-aws-modules/eks/aws"
-    cluster_name    = var.cluster_name
-    cluster_version = module.vpc.public_subnets
-    vpc_id          = module.vpc.vpc_id  
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "18.29.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+    }
+  }
 }
