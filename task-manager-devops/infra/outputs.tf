8cb954bbb0a1d3a99fbb04d6b01e4b48ac4797c1
Yakir Adela <yakirade@gmail.com>
2025-07-05 17:37:49 +0300

    Update outputs.tf

--- task-manager-devops/infra/outputs.tf
+++ task-manager-devops/infra/outputs.tf
@@ -6,3 +6,7 @@ output "ecr_repository_url" {
   description = "URL of the ECR repository"
   value       = aws_ecr_repository.task_api.repository_url
 }
+
+output "cluster_name" {
+  value = module.eks.cluster_id
+}
