f9924957045b7aa153263154b4e53e4edb9936d7
Yakir Adela <yakirade@gmail.com>
2025-07-05 17:28:50 +0300

    Update main.tf

--- task-manager-devops/infra/main.tf
+++ task-manager-devops/infra/main.tf
@@ -6,9 +6,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = ">= 5.33.0"
     }
   }
 
-  required_version = ">= 1.3"
+  required_version = ">= 1.3.0"
 }
