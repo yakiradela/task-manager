3330bb66460192bdb4f59bc5371a99b077dda323
Yakir Adela <yakirade@gmail.com>
2025-07-05 17:27:40 +0300

    Update eks-cluster.tf

--- task-manager-devops/infra/eks-cluster.tf
+++ task-manager-devops/infra/eks-cluster.tf
@@ -12,10 +12,10 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "19.21.0"
+  version         = "20.8.4"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
